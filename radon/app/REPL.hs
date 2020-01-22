001ac9e6d9bc3ec49e96473cf348dde2a0df22b1
Ashley Towns <mail@ashleytowns.id.au>
2020-01-22 11:18:52 +1100

    cleanup

--- radon/app/REPL.hs
+++ radon/app/REPL.hs
@@ -6,81 +6,34 @@ module Main where
 import Options.Applicative
 import System.Console.Haskeline
 
-import Control.Monad(when)
 import Text.Pretty.Simple
 
-import Text.PrettyPrint (render)
 import Parser
-import qualified Parser.Common as PC
 import Parser.TopLevel (pTopLevel)
 import Parser.Statement (pStmt)
-import Parser.Expression (pExpr)
-import CodeGen.C.Pretty
-import CodeGen.C.TopLevel
 
 import TypeCheck
-
-import AST.Phases.Undecorated
-
-import Rewriters.Rewriter
-import Rewriters.C.FunctionAlias
-import Analyzers.Analyzer
-import Analyzers.Graphviz
 
 data Options = Options { verbose    :: Bool
                        } deriving (Show)
 
-extractors :: [FunctionAliases]
-extractors = [functionAliases]
-
-analyzers :: [Graph]
-analyzers = [graph]
-
--- concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
--- {-# INLINE concatMapM #-}
--- concatMapM op = foldr f (pure [])
---  where
---   f x xs = do
---     x' <- op x
---     if null x' then xs else do
---       xs' <- xs
---       pure $ x'++xs'
-
--- evalFile :: FilePath -> IO String
--- evalFile fp = do
---   tys <- parseFile "stdlib/types.ra"
---   console <- parseFile "stdlib/console.ra"
---   ast <- parseFile fp
---   _ <- analyze (head analyzers) (toUndecorated <$> ast)
---   let tree = tys ++ console ++ ast
---   -- tree <- foldM (\tree rewriter -> rewrite rewriter tree) (tys ++ console ++ ast) resolvers
---   preamb <- concatMapM (`extract` tree) extractors
---   let ctree = evalTopLevels tree
---   return $ render $ pretty $ prependC ctree preamb
-
-
-evalShowable :: [PC.Parser Text] -> Text -> Text
-evalShowable [] _       = "no parsers given.."
-evalShowable (x:[]) inp = bifold $ parseText x "<input>" inp
-evalShowable (x:xs) inp = bifoldMap (\_ -> evalShowable xs inp) id $ parseText x "<input>" inp
-
 evalStatement :: Text -> Text
-evalStatement inp = 
+evalStatement inp =
   case parseText pStmt "<input>" inp of
     Left err -> "Syntax error: " <> err
-    Right x  -> 
+    Right x  ->
       case typeCheckStatement x of
         Left err -> "Type error: " <> err
-        Right x -> toS $ pShow x
+        Right x' -> toS $ pShow x'
 
 evalTL :: Text -> Text
-evalTL inp = 
+evalTL inp =
   case parseText pTopLevel "<input>" inp of
-    Left err -> evalStatement inp -- "Syntax error: " <> err
-    Right x  -> 
+    Left _ -> evalStatement inp
+    Right x  ->
       case traverse typeCheckTopLevel x of
         Left err -> "Type error: " <> err
-        Right x -> toS $ pShow x
+        Right x' -> toS $ pShow x'
 
 program :: Options -> IO ()
 program Options{..} = do
